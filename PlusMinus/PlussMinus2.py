69aae3e169fee4d125c1d4f0a0e7960176a65650
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2022-05-28 09:42:18 +0700

    My Edit Code

--- PlusMinus/PlussMinus2.py
+++ PlusMinus/PlussMinus2.py
@@ -1,15 +1,9 @@
-
-
+import math
 def plusMinus(arr):
-    positive,negative,zero, ran = [i for i in arr if i > 0],[i for i in arr if i < 0],[i for i in arr if i == 0], len(arr)
-    print(len(positive)/ran)
-    print(len(negative)/ran)
-    print(len(zero)/ran)
-        
-            
+    positive, negative, zero, ran = [i for i in arr if i > 0], [i for i in arr if i < 0],[i for i in arr if i == 0], len(arr)
+    print('{}\n{}\n{}'.format(len(positive)/ran, len(negative)/ran, len(zero)/ran))
+ 
 if __name__ == '__main__':
-    n = int(input().strip())
-
-    arr = list(map(int, input().rstrip().split()))
-
-    plusMinus(arr)
+   n = int(input().strip())
+   arr = list(map(int, input().rstrip().split()))
+   plusMinus(arr)
