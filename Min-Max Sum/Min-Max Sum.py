6ffc3e93204c8f36a279a76d09904daec7b95c2b
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2022-05-28 09:27:07 +0700

    edited

--- Min-Max Sum/Min-Max Sum.py
+++ Min-Max Sum/Min-Max Sum.py
@@ -13,11 +13,11 @@ import sys
 #
 
 def miniMaxSum(arr):
-    sum_arr = sum(arr)
-    sum_list = []    
-    for i in arr:
-        sum_list.append(sum_arr-i)
-    print(min(sum_list),max(sum_list))
+    sum_arr = sum(arr)  #sum all list on arr
+    sum_list = []    # define empty array for sum of list
+    for i in arr: 
+        sum_list.append(sum_arr-i) # this loop every sum with minus i on iterate loop
+    print(min(sum_list),max(sum_list)) # search min and max on sum of list
    
 
 if __name__ == '__main__':
