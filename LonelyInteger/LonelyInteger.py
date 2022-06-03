1aefe53ffcc517dbd3996abbdb1175ba25c8ae44
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2022-06-03 07:14:54 +0700

    Update LonelyInteger.py

--- LonelyInteger/LonelyInteger.py
+++ LonelyInteger/LonelyInteger.py
@@ -1,32 +1,19 @@
-#!/bin/python3
-
+#Import Libary
 import math
 import os
 import random
 import re
 import sys
 
-#
-# Complete the 'lonelyinteger' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER_ARRAY a as parameter.
-#
-
+#Function
 def lonelyinteger(a):
    return  2*(sum(set(a))) - sum(a) 
                 
- 
-
+#Main Program
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
+    fptr = open(os.environ['OUTPUT_PATH'], 'w') #to execute output on my machine *just delete if error
     n = int(input().strip())
-
     a = list(map(int, input().rstrip().split()))
-
     result = lonelyinteger(a)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
