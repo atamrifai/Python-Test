44e29311c0b4f570c53296dab2356922944d197a
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2022-06-03 07:25:29 +0700

    Update FlippingBits.py

--- FlippingBits/FlippingBits.py
+++ FlippingBits/FlippingBits.py
@@ -6,26 +6,18 @@ import random
 import re
 import sys
 
-#
-# Complete the 'flippingBits' function below.
-#
-# The function is expected to return a LONG_INTEGER.
-# The function accepts LONG_INTEGER n as parameter.
-#
 
 def flippingBits(n):
    return (2**32 - 1) - n
+   #explanation:
+   #don't flip the integer but minus it by n
+   #2**32 = maximum integer on 32 Bit. just min by n. because output on 32 Bit integer
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     q = int(input().strip())
-
     for q_itr in range(q):
         n = int(input().strip())
-
         result = flippingBits(n)
-
         fptr.write(str(result) + '\n')
-
     fptr.close()
