14eafaa9523a32082540a1ddd5ff39440d1246cd
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2023-03-24 11:47:07 +0700

    Update TimeConversion.py

--- TimeConversion/TimeConversion.py
+++ TimeConversion/TimeConversion.py
@@ -12,3 +12,19 @@ import sys
 # The function is expected to return a STRING.
 # The function accepts STRING s as parameter.
 #
+
+def timeConversion(s):
+    if s[-2:] == "AM" and s[0:2] == "12":
+        return("00" + s[2:-2])
+    elif s[-2:] == "PM" and s[0:2] == "12" or s[-2:] == "AM":
+        return(str(s[:-2]))
+    elif s[-2:] == "PM":
+        return(str(int(s[:2])+12)+s[2:-2])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+    result = timeConversion(s)
+    fptr.write(result + '\n')
+    fptr.close()
