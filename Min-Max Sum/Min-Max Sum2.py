f68381edc86a06a4557bc72f15288661f1f9dd9c
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2023-03-26 10:01:40 +0700

    Update Min-Max Sum2.py

--- Min-Max Sum/Min-Max Sum2.py
+++ Min-Max Sum/Min-Max Sum2.py
@@ -16,8 +16,4 @@ def miniMaxSum(arr):
     print(sum(arr)-max(arr), sum(arr)-min(arr))
    
 
-if __name__ == '__main__':
 
-    arr = list(map(int, input().rstrip().split()))
-
-    miniMaxSum(arr)
