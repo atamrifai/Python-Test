80905b9e6da2ef1840922a4417b2f57a875c37e7
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2023-03-26 10:03:24 +0700

    Update Min-Max Sum2.py

--- Min-Max Sum/Min-Max Sum2.py
+++ Min-Max Sum/Min-Max Sum2.py
@@ -12,8 +12,7 @@ import sys
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 
-def miniMaxSum(arr):
-    print(sum(arr)-max(arr), sum(arr)-min(arr))
+
    
    
 if __name__ == '__main__':
