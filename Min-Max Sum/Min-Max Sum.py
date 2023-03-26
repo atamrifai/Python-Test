1a0dc54e6f94ad71e524d2c87e48b262a44e02f1
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2023-03-26 10:04:23 +0700

    Update Min-Max Sum.py

--- Min-Max Sum/Min-Max Sum.py
+++ Min-Max Sum/Min-Max Sum.py
@@ -11,7 +11,12 @@ import sys
 #
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
-
+def miniMaxSum(arr):
+    sum_arr = sum(arr)  #sum all list on arr
+    sum_list = []    # define empty array for sum of list
+    for i in arr: 
+        sum_list.append(sum_arr-i) # this loop every sum with minus i on iterate loop
+    print(min(sum_list),max(sum_list)) # search min and max on sum of list
 
    
 
