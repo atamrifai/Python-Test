3d2554a86ffae0bb700b1eee120eb9a7e97649fe
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2023-09-13 10:29:02 +0700

    Update LonelyInteger.py

--- LonelyInteger/LonelyInteger.py
+++ LonelyInteger/LonelyInteger.py
@@ -10,10 +10,4 @@ def lonelyinteger(a):
    return  2*(sum(set(a))) - sum(a) 
                 
 #Main Program
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w') #to execute output on my machine *just delete if error
-    n = int(input().strip())
-    a = list(map(int, input().rstrip().split()))
-    result = lonelyinteger(a)
-    fptr.write(str(result) + '\n')
-    fptr.close()
+
