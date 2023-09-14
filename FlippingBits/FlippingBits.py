15b05b19c6451df4ac989b02b4a7bbdb415032c1
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2023-09-14 12:58:55 +0700

    Update FlippingBits.py

--- FlippingBits/FlippingBits.py
+++ FlippingBits/FlippingBits.py
@@ -13,11 +13,3 @@ def flippingBits(n):
    #2**32 = maximum integer on 32 Bit. just min by n. because output on 32 Bit integer
 
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    q = int(input().strip())
-    for q_itr in range(q):
-        n = int(input().strip())
-        result = flippingBits(n)
-        fptr.write(str(result) + '\n')
-    fptr.close()
