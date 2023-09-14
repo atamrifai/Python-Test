079a5fdc7916160fa5300864b80376d82beba3df
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2023-09-14 12:58:43 +0700

    Update FlippingBits.py

--- FlippingBits/FlippingBits.py
+++ FlippingBits/FlippingBits.py
@@ -6,7 +6,11 @@ import random
 import re
 import sys
 
-
+def flippingBits(n):
+   return (2**32 - 1) - n
+   #explanation:
+   #don't flip the integer but minus it by n
+   #2**32 = maximum integer on 32 Bit. just min by n. because output on 32 Bit integer
 
 
 if __name__ == '__main__':
