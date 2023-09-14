cd3eed72acf58a536757102500e4fe1d061dd133
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2023-09-14 12:58:33 +0700

    Update FlippingBits.py

--- FlippingBits/FlippingBits.py
+++ FlippingBits/FlippingBits.py
@@ -7,11 +7,7 @@ import re
 import sys
 
 
-def flippingBits(n):
-   return (2**32 - 1) - n
-   #explanation:
-   #don't flip the integer but minus it by n
-   #2**32 = maximum integer on 32 Bit. just min by n. because output on 32 Bit integer
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
