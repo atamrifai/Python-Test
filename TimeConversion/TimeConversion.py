826bdfa8f718820be3b8c1e71fc2dfa1bedb8cf6
Atam Rifa'i Sujiwanto <58683035+atamrifai@users.noreply.github.com>
2023-09-28 16:11:35 +0700

    Update TimeConversion.py

--- TimeConversion/TimeConversion.py
+++ TimeConversion/TimeConversion.py
@@ -24,5 +24,8 @@ def timeConversion(s):
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-
+    s = input()
+    result = timeConversion(s)
+    fptr.write(result + '\n')
+    fptr.close()
 
